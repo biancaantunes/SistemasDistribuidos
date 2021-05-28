76fccbcd14348eaa97ba65a26ad7c11456de6b0e
Bianca Antunes <62080345+biancaantunes@users.noreply.github.com>
2021-05-28 12:32:41 -0300

    Atividades

--- RabbitMQ/ProjAvaliacaoRabbit/src/main/java/leo/constant/Constant.java
+++
@@ -1,6 +0,0 @@
-package leo.constant;
-
-public class Constant {
-    public static final String API_URL = "projavaliacaorabbit/api/v1/";
-    public static final String API_AVALIACAO =  API_URL + "avaliacao";
-}
